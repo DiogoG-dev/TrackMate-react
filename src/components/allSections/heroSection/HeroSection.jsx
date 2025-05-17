da3533bf79a72d4fc262c831e6a0bbf5edd043b3
DiogoGabriel <diogogabriel.tech@gmail.com>
2025-05-17 14:47:17 -0300

    Some corrections

---
+++ src/components/allSections/heroSection/HeroSection.jsx
@@ -0,0 +1,17 @@
+import './HeroSection.css'
+
+export function HeroSection() {
+    return (
+        <section class="hero-section">
+            <div class="hero-content">
+                <div class="product-presentation">
+                    <span class="hero-text">Tenha uma rotina diária</span>
+                    <h1 class="hero-main-title">TrackMade 0.4 XE</h1>
+                    <h2 class="hero-secondary-title">O smartwatch que motiva você em cada passo</h2>
+                    <a href="./index.html" class="buy-button">Comprar agora</a>
+                </div>
+                <div class="product-image"></div>
+            </div>
+        </section>
+    )
+}
--- src/components/buyButton/BuyButton.jsx
+++ src/components/buyButton/BuyButton.jsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
 import './BuyButton.css';
+import { Link } from 'react-router-dom';
 
 export function BuyButton() {
     return (
