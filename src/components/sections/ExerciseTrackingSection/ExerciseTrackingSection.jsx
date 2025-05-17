8f803979ef1edf6d850d24c1c37affb7fee2274d
DiogoGabriel <diogogabriel.tech@gmail.com>
2025-05-17 17:50:44 -0300

    Creating ExerciseTrackingSection component

---
+++ src/components/sections/ExerciseTrackingSection/ExerciseTrackingSection.jsx
@@ -0,0 +1,19 @@
+import './ExerciseTrackingSection.css';
+
+export function ExerciseTrackingSection() {
+    return (
+        <section className="exercise-tracking-section">
+            <div className="exercise-tracking-content">
+                <h2><i className="fa-solid fa-dumbbell"></i>ACOMPANHAMENTO DE EXERCÍCIOS</h2>
+                <h3>Ajuste sua rotina com estatísticas precisas de treino</h3>
+                <p>
+                    O <b>TrackMade 0.4 XE</b> oferece monitoramento avançado, gráficos detalhados e insights em tempo real para otimizar seu 
+                    desempenho em cada sessão de treino.
+                </p>
+            </div>
+            <div className="gradient-background-product">
+                <img className="smartphoneSmarwatch" src="/TrackMate-react/imgs/folds/smartphoneSmarwatch.avif" alt="" />
+            </div>
+        </section>
+    )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import './Home.css';
 import { Header } from '../../components/layouts/Header/Header.jsx';
 import { Footer } from '../../components/layouts/Footer/Footer.jsx';
 import { HeroSection } from '../../components/sections/HeroSection/HeroSection.jsx';
+import { ExerciseTrackingSection } from '../../components/sections/ExerciseTrackingSection/ExerciseTrackingSection.jsx';
 
 export function Home() {
     return (
         <>
             <Header />
             <HeroSection />
+            <ExerciseTrackingSection />
             <Footer />
         </>
     )
