64eb1e71da153f4a1aac3fca7a5ecb5c55bb747d
DiogoGabriel <diogogabriel.tech@gmail.com>
2025-05-19 16:38:35 -0300

    Some corrections

--- src/components/sections/DiscountOffer/DiscountOffer.jsx
+++ src/components/sections/DiscountOffer/DiscountOffer.jsx
@@ -4,12 +4,11 @@ export function DiscountOffer() {
     return (
         <section className="discount-offer">
             <h2>Assine e ganhe 15% de desconto na primeira compra</h2>
-            <label for='email'>Email *</label>
             <form action="">
+                <label htmlFor='email'>Email *</label>
                 <input type="email" id='email' placeholder="Assine aqui" required />
-                <button className='inside-button' type="submit">Assinar</button>
+                <button type="submit">Assinar</button>
             </form>
-            <button className='outside-button' type="submit">Assinar</button>
         </section>
     )
 }
