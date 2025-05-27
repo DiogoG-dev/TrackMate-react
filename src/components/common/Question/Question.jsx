aba9120b941550d17c1904cdddf8219ec8b567a1
DiogoGabriel <diogogabriel.tech@gmail.com>
2025-05-27 15:16:25 -0300

    Some corrections and refacturing Faq component

--- src/components/common/Question/Question.jsx
+++ src/components/common/Question/Question.jsx
@@ -4,15 +4,15 @@ import { useEffect } from 'react';
 export function Question({ questionContentId, arrowId, responseId, question, response }) {
 
     function toggleQuestion() {
-        const arrow = document.getElementById(arrowId)
-        const response = document.getElementById(responseId)
+        const arrow = document.getElementById(`arrow-${arrowId}`)
+        const response = document.getElementById(`response-${responseId}`)
 
         arrow.classList.toggle('change');
         response.classList.toggle('show');
     }
 
     useEffect(() => {
-        const questionContainer = document.getElementById(questionContentId)
+        const questionContainer = document.getElementById(`question-content-${questionContentId}`)
         if(questionContainer) {
             questionContainer.addEventListener('click', toggleQuestion)
         }
@@ -20,11 +20,11 @@ export function Question({ questionContentId, arrowId, responseId, question, res
 
     return (
         <div className='question-container'>
-            <div className="question-content" id={questionContentId}>
+            <div className="question-content" id={`question-content-${questionContentId}`}>
                 <h4 className='question'>{question}</h4>
-                <span id={arrowId}><i class="fa-solid fa-angle-down"></i></span>
+                <span id={`arrow-${arrowId}`}><i className="fa-solid fa-angle-down"></i></span>
             </div>
-            <p className='response' id={responseId}>{response}</p>
+            <p className='response' id={`response-${responseId}`}>{response}</p>
         </div>
     )
 }
