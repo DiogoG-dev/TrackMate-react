abb576f9ea3713d1d57083b108071172d009269c
DiogoGabriel <diogogabriel.tech@gmail.com>
2025-05-26 22:28:48 -0300

    Creating Faq page

--- src/pages/Faq/Faq.jsx
+++ src/pages/Faq/Faq.jsx
@@ -1,8 +1,11 @@
 import './Faq.css';
 import { Header } from '../../components/layouts/Header/Header.jsx';
 import { Footer } from '../../components/layouts/Footer/Footer.jsx';
+import { Question } from '../../components/common/Question/Question.jsx';
+import faqData from '../../data/faq.json'
 
 export function Faq() {
+    console.log(faqData)
     return (
         <>
             <Header />
@@ -10,10 +13,8 @@ export function Faq() {
                 <div className="content">
                     <h2>FAQ</h2>
                     <h3>Perguntas frequentes</h3>
-                    <div className='test-container open'>
-                        <h4 className='title-1'>Conteúdo teste?</h4>
-                        <p>Sim, esse conteúdo é um teste!</p>
-                    </div>
+                    <Question questionContentId='question-content-1' arrowId='arrow-1' responseId='response-1' question={faqData[0].question} response={faqData[0].response}  />
+                    <Question questionContentId='question-content-2' arrowId='arrow-2' responseId='response-2' question='a' response='f' />
                 </div>
             </main>
             <Footer />
