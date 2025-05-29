1da248dd93c93f2e92b229afb93f50b2eef1bc47
DiogoGabriel <diogogabriel.tech@gmail.com>
2025-05-29 16:39:42 -0300

    Creating carousel images

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,7 +1,7 @@
 import './ProductPage.css'
 import { Header } from '../../components/layouts/Header/Header.jsx';
 import { Footer } from '../../components/layouts/Footer/Footer.jsx';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Question } from '../../components/common/Question/Question.jsx';
 import termsData from '../../data/terms.json';
 
@@ -17,10 +17,21 @@ export function ProductPage() {
 
                     <div className="product-apresentation">
                         <div className="product-apresentation-image">
-                            <div className="main-image"></div>
-                            <div className="image-carousel">1</div>
+                            <div className="main-image"><img className='bracelet-color' id='bracelet-color' src={`/TrackMate-react/imgs/folds/bracelete${color}.png`} alt="" /></div>
+                            <div className="image-carousel">
+                                <input type="radio" className="bracelet-colors" id="bracelet-white" name="bracelet-color" onChange={() => document.getElementById('bracelet-color').src = "/TrackMate-react/imgs/folds/braceleteBranco.png"} />
+                                <label htmlFor="bracelet-white" className="bracelet-labels bracelet-label-white"></label>
+                                <input type="radio" className='bracelet-colors' id='bracelet-black' name='bracelet-color' onClick={() => document.getElementById('bracelet-color').src = "/TrackMate-react/imgs/folds/braceletePreto.png"} />
+                                <label htmlFor="bracelet-black" className='bracelet-labels bracelet-label-black'></label>
+                                <input type="radio" className='bracelet-colors' id='bracelet-blue' name='bracelet-color' onClick={() => document.getElementById('bracelet-color').src = "/TrackMate-react/imgs/folds/braceleteAzul.png"} />
+                                <label htmlFor="bracelet-blue" className='bracelet-labels bracelet-label-blue'></label>
+                                <input type="radio" className='bracelet-colors' id='bracelet-purple' name='bracelet-color' onClick={() => document.getElementById('bracelet-color').src = "/TrackMate-react/imgs/folds/braceleteRoxo.png"} />
+                                <label htmlFor="bracelet-purple" className='bracelet-labels bracelet-label-purple'></label>
+                                <input type="radio" className='bracelet-colors' id='bracelet-turquoise' name='bracelet-color' onClick={() => document.getElementById('bracelet-color').src = "/TrackMate-react/imgs/folds/braceleteTurquesa.png"} />
+                                <label htmlFor="bracelet-turquoise" className='bracelet-labels bracelet-label-turquoise'></label>
+                            </div>
                         </div>
-                        <div className="product-description">2</div>
+                        <div className="product-description">O TrackMate 0.4 XE tem pulseira de silicone ajustável, é leve e resistente. Evite calor e água excessivos. Limpe com pano seco, carregue apenas com o cabo magnético incluso e utilize o app TrackMate Health para configurar e acompanhar seus dados.</div>
                     </div>
                     
                     <div className="information-purchase-and-terms">
@@ -40,7 +51,7 @@ export function ProductPage() {
                                         <span className="tooltip">Branco</span>
                                     </div>
                                     <div className="color-tooltip">
-                                        <input type="radio" className='colors' id='black' name='color'onClick={() => setColor('Preto')} />
+                                        <input type="radio" className='colors' id='black' name='color' onClick={() => setColor('Preto')} />
                                         <label htmlFor="black" className='labels label-black'></label>
                                         <span className="tooltip">Preto</span>
                                     </div>
