1b013c6edf6fb170883428a5e4eafffd8fc62de5
DiogoGabriel <diogogabriel.tech@gmail.com>
2025-06-02 19:18:42 -0300

    Adding element emptyBag to verify is exist elements in localStorage

--- src/pages/Bag/Bag.jsx
+++ src/pages/Bag/Bag.jsx
@@ -2,15 +2,26 @@ import './Bag.css';
 import { Header } from '../../components/layouts/Header/Header.jsx';
 import { Footer } from '../../components/layouts/Footer/Footer.jsx';
 import { Item } from '../../components/common/Item/Item.jsx';
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 export function Bag() {
+    const [haveItems, setHaveItems] = useState();
+
+    useEffect(() => {
+        if (localStorage.length > 0) {
+            setHaveItems(true)
+        } else {
+            setHaveItems(false)
+        }
+    }, [])
     return (
         <>
             <Header />
             <div className="bag-container">
-                <div className="bag-content">
+                <div className="bag-content" style={{display: haveItems ? 'flex' : 'none'}}>
                     <div className="bag-items">
-                        <h2>Minha Sacola</h2>
+                        <h2 className='my-bag'>Minha Sacola</h2>
                         <div className="items">
                             <Item />
                         </div>
@@ -39,6 +50,13 @@ export function Bag() {
                         </div>
                         <button className='pay-button'>Pagar Agora</button>
                         <span className='secure-checkout'><i className="fa-solid fa-lock"></i>Checkout Seguro</span>
+                    </div>
+                </div>
+                <div className="empty-bag-container" style={{display: haveItems ? 'none' : 'flex'}}>
+                    <h2 className='my-bag'>Minha Sacola</h2>
+                    <div className="empty-bag-content">
+                        <h3 className='empty-bag-title'>A sacola está vazia</h3>
+                        <Link to='/homePage' className='continue-browsing'>Continuar navegando</Link>
                     </div>
                 </div>
             </div>
