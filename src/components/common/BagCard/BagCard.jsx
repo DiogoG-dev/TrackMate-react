3011cc7bf96aa9f8e6d25db9e68240dfc9d639fe
DiogoGabriel <diogogabriel.tech@gmail.com>
2025-06-02 16:39:04 -0300

    Some corrections of desing

--- src/components/common/BagCard/BagCard.jsx
+++ src/components/common/BagCard/BagCard.jsx
@@ -2,8 +2,10 @@ import './BagCard.css';
 import { useEffect, useState } from 'react';
 import { Header } from '../../layouts/Header/Header.jsx';
 import { Footer } from '../../layouts/Footer/Footer.jsx';
+import { Link } from 'react-router-dom';
 
 export function BagCard() {
+    const [amount, setAmount] = useState(1)
     const [items, setItems] = useState({});
     const [totalAmount, setTotalAmount] = useState(0);
     const [totalPrice, setTotalPrice] = useState(0);
@@ -37,15 +39,19 @@ export function BagCard() {
                     <div className="bag-items">
                         <h2>Minha Sacola</h2>
                         <div className="items">
-                            <img src="/TrackMate-react/imgs/bracelets/braceleteAzul.png" alt="" className="img-item" />
+                            <Link to="/productPage"><img src="/TrackMate-react/imgs/bracelets/braceleteAzul.png" alt="" className="img-item" /></Link>
                             <div className="info-item">
-                                <h3>TrackMate 0.4 XE</h3>
+                                <h3><Link to="/productPage">TrackMate 0.4 XE</Link></h3>
                                 <span className='unit-value'>R$ 85,00</span>
                                 <span className="color">Cor: Azul</span>
                             </div>
-                            <div className="amount">2</div>
+                            <div className="choose-amount">
+                                    <button className={`minus ${amount === 1 ? 'minus-gray' : ''}`} id="minus" onClick={() => setAmount(amount - 1)} disabled={amount === 1}><i className="fa-solid fa-minus"></i></button>
+                                    <div className="amount">{amount}</div>
+                                    <button className="plus" onClick={() => setAmount(amount + 1)}><i className="fa-solid fa-plus"></i></button>
+                                </div>
                             <div className="total-value-item">R$ 170,00</div>
-                            <div className="trash">Lixo</div>
+                            <div className="trash"><i class="fas fa-trash"></i></div>
                         </div>
                         <div className='promotional-code'>
                             <label className="label-promotional-code">Insira o código promocional</label>
@@ -56,7 +62,7 @@ export function BagCard() {
                         </div>
                         <div className="observations">
                             <label className="label-observations">Adicione uma observação</label>
-                            <textarea type="text" className="input-observations" />
+                            <textarea type="text" className="input-observations" placeholder='Por exemplo: Não entregar na terça'/>
                         </div>
                     </div>
                     <div className="order-summary">
@@ -71,7 +77,7 @@ export function BagCard() {
                             <span className="total-value">R$ 85,00</span>
                         </div>
                         <button className='pay-button'>Pagar Agora</button>
-                        <span className='secure-checkout'>Checkout Seguro</span>
+                        <span className='secure-checkout'><i class="fa-solid fa-lock"></i> Checkout Seguro</span>
                     </div>
                 </div>
             </div>
