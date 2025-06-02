082ca9c16b8c7087c4bfafd4634accdee1073bf9
DiogoGabriel <diogogabriel.tech@gmail.com>
2025-06-01 21:59:54 -0300

    Creating bag styling

--- src/components/common/BagCard/BagCard.jsx
+++ src/components/common/BagCard/BagCard.jsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from 'react';
 import './BagCard.css';
+import { useEffect, useState } from 'react';
+import { Header } from '../../layouts/Header/Header.jsx';
+import { Footer } from '../../layouts/Footer/Footer.jsx';
 
 export function BagCard() {
     const [items, setItems] = useState({});
@@ -28,37 +30,52 @@ export function BagCard() {
     }, []);
 
     return (
-        <div className="bag-card-container">
-            <div className="bag-card-header">
-                <div className="bag-card-header-content">
-                    <h2>Sacola</h2>
-                    <span>{`(${totalAmount} itens)`}</span>
-                </div>
-                <div className="close-card-bag-card">
-                    <div className="bar1"></div>
-                    <div className="bar2"></div>
-                </div>
-            </div>
-
-            <div className="bag-content">
-                {Object.entries(items).map(([key, item]) => (
-                    <div key={key} className="bag-item">
-                        <h3>TrackMate 0.4 XE</h3>
-                        <span>R$ 85,00</span><br />
-                        <span>Cor {item.color}</span><br />
-                        <span>Qtd: {item.amount}</span><br />
-                        <span>R$ {item.price.toFixed(2)}</span><br /><br />
+        <>
+            <Header />
+            <div className="bag-container">
+                <div className="bag-content">
+                    <div className="bag-items">
+                        <h2>Minha Sacola</h2>
+                        <div className="items">
+                            <img src="/TrackMate-react/imgs/bracelets/braceleteAzul.png" alt="" className="img-item" />
+                            <div className="info-item">
+                                <h3>TrackMate 0.4 XE</h3>
+                                <span className='unit-value'>R$ 85,00</span>
+                                <span className="color">Cor: Azul</span>
+                            </div>
+                            <div className="amount">2</div>
+                            <div className="total-value-item">R$ 170,00</div>
+                            <div className="trash">Lixo</div>
+                        </div>
+                        <div className='promotional-code'>
+                            <label className="label-promotional-code">Insira o código promocional</label>
+                            <div className="input-button-promotional-code">
+                                <input type="text" className="input-promocional-code" />
+                                <button className="enter-promocional-code">OK</button>
+                            </div>
+                        </div>
+                        <div className="observations">
+                            <label className="label-observations">Adicione uma observação</label>
+                            <textarea type="text" className="input-observations" />
+                        </div>
                     </div>
-                ))}
-            </div>
-
-            <div className="bag-card-footer">
-                <div className="bag-card-footer-content">
-                    <h3>Subtotal</h3>
-                    <span>{`R$ ${totalPrice.toFixed(2)}`}</span>
+                    <div className="order-summary">
+                        <h2>Resumo do Pedido</h2>
+                        <div className="subtotal">
+                            <h3>Subtotal</h3>
+                            <span className="subtotal-value">R$ 85,00</span>
+                        </div>
+                        <h3 className='estimated-delivery'>Estimar Entrega</h3>
+                        <div className='total-container'>
+                            <h2>Total</h2>
+                            <span className="total-value">R$ 85,00</span>
+                        </div>
+                        <button className='pay-button'>Pagar Agora</button>
+                        <span className='secure-checkout'>Checkout Seguro</span>
+                    </div>
                 </div>
-                <button className="checkout">Checkout</button>
             </div>
-        </div>
+            <Footer />
+        </>
     );
 }
