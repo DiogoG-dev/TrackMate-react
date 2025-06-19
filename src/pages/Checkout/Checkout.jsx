dfa730b364b8d97915f0ff4cfbe446eb321dc09b
DiogoGabriel <diogogabriel.tech@gmail.com>
2025-06-19 18:30:47 -0300

    Adding checkout initial structury

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -5,39 +5,43 @@ export function Checkout() {
     return (
         <>
             <header className='header-checkout-container'>
-                <Link to={'/'} className="logo"><div className="outer-circle"><div className="inner-circle"></div></div><span>TrackMate</span></Link>
+                <Link to={'/'} className="logo"><div className="outer-circle"><div className="inner-circle"></div></div><span>TrackMatePay</span></Link>
             </header>
             <main className='checkout-container'>
-                <div className="checkout-content">
-                    <div className="checkout-flow-container">
-                        <div className="payment-center-title">
-                            <h2>Central de Pagamento</h2>
-                        </div>
-                        <div className="checkout-flow">
-                            <div className='flow payment-method'>Formas de Pagamento</div>
-                            <div className="flow payment-information">Informações de Pagamento</div>
-                            <div className='flow payment-confirmation'>Confirmação</div>
-                            <div className="flow tracking">Rastreio</div>
-                        </div>
-                    </div>
-                    <div className="checkout-flow-contents">
-                        <div className="payment-method-container">
-                            <h3>Formas de Pagamento</h3>
-                            <div className="payments ayment-pix">Pix</div>
-                            <div className="payments payment-card">Cartão</div>
-                            <div className="payments payment-ticket">Boleto</div>
-                        </div>
-                        <div className="payment-information-container">
-                            <div className="pix-qrcode"></div>
-                            <div className="card-informations"></div>
-                            <div className="ticket-code"></div>
-                        </div>
-                        <div className="payment-confirmation-container"></div>
-                        <div className="tracking-container"></div>
+                <div className="payment-methods">
+                    <h2>Formas de Pagamento</h2>
+                    <div className='methods'>
+                        <div className="card-container">Cartão de Crédito</div>
+                        <div className="ticket-bank-container">Boleto Bancário</div>
+                        <div className="pix-container">Pix</div>
                     </div>
                 </div>
+                <div className="form-container"></div>
+                <div className="order-summary">
+                        <h2 className='title'>Resumo do Pedido</h2>
+                        <div className="product-container">
+                            <h4>Itens</h4>
+                            <div className="itens">
+                                <span>2x TrackMate Azul</span>
+                                <span>1x TrackMate Preto</span>
+                                <span>3x TrackMate Turquesa</span>
+                            </div>
+                        </div>
+                        <div className="chosen-method">
+                            <h4>Forma de Pagamento</h4>
+                            <span>Cartão de Crédito - 2x de R$ 75,00</span>
+                        </div>
+                        <div className='total-container'>
+                            <h2>Total</h2>
+                            <span className="total-value">R$ 150,00</span>
+                        </div>
+                        <Link to="/pageNotFound" className='pay-button'>Finalizar Compra</Link>
+                    </div>
             </main>
-            <footer className='footer-checkout-container'></footer>
+            <footer className='footer-checkout-container'>
+                <span>TrackMatePay Tecnologia LTDA.</span>
+                <span>Checkout 100% seguro.</span>
+            </footer>
         </>
     )
 }
